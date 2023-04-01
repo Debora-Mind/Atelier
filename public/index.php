6ad510cfcaca474ff0750c1ad7f05c74560d9530
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-04-01 17:49:24 -0300

    Alterações para rodar no servidor

--- public/index.php
+++ public/index.php
@@ -10,7 +10,6 @@ use Psr\Http\Server\RequestHandlerInterface;
 $caminho = $_SERVER['PATH_INFO'] ?? '/';
 
 $rotas = require __DIR__ . '/../config/routes.php';
-
 if (!array_key_exists($caminho, $rotas)) {
     http_response_code(404);
     exit();
