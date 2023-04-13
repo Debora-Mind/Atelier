dedbf66323e498d385c052abf4cad45e8e49f1e7
Débora <debora.almeida.de.mello@gmail.com>
2023-04-13 13:42:52 -0300

    Ajuste no front do formulário de modelos

--- view/Modelos/formulario.php
+++ view/Modelos/formulario.php
@@ -2,9 +2,9 @@
 <?php include __DIR__ . '/../Componentes/navbar.php'; ?>
 
     <form action="/salvar-modelo<?= isset($modelo) ? '?id=' . $modelo->getId() : ''; ?>"
-          method="post">
-        <div class="form-group gx-2 justify-content-md-start col-3">
-            <div class="col col-md-auto">
+          method="post" class="justify-content">
+        <div class="form-group justify-content-start row">
+            <div class="col col-md-3 me-5">
                 <label for="modelo">Modelo</label>
                 <input type="text"
                        autofocus
@@ -13,7 +13,7 @@
                        class="form-control"
                        value="<?= isset($modelo) ? $modelo->getModelo() : ''; ?>">
             </div>
-            <div class="col col-md-auto">
+            <div class="col col-md-3 mx-5">
                 <label for="producao">Rel.Produção</label>
                 <input type="number"
                        id="producao"
@@ -21,7 +21,7 @@
                        class="form-control"
                        value="<?= isset($modelo) ? $modelo->getProducao() : ''; ?>">
             </div>
-            <div class="col col-md-auto">
+            <div class="col col-md-3 mx-5">
                 <label for="sublote">Sublote</label>
                 <input type="number"
                        id="sublote"
@@ -29,7 +29,7 @@
                        class="form-control"
                        value="<?= isset($modelo) ? $modelo->getSublote() : ''; ?>">
             </div>
-            <div class="col col-md-auto">
+            <div class="col col-md-3 me-5">
                 <label for="quantidade">Quantidade</label>
                 <input type="number"
                        id="quantidade"
@@ -37,7 +37,7 @@
                        class="form-control"
                        value="<?= isset($modelo) ? $modelo->getQuantidade() : ''; ?>">
             </div>
-            <div class="col col-md-auto">
+            <div class="col col-md-3 mx-5">
                 <label for="valor">Valor</label>
                 <input type="number"
                        step="0.01"
@@ -46,7 +46,7 @@
                        class="form-control"
                        value="<?= isset($modelo) ? $modelo->getValor() : ''; ?>">
             </div>
-            <div class="col col-md-auto">
+            <div class="col col-md-3 mx-5">
                 <label for="semana">Semana</label>
                 <input type="number"
                        id="semana"
@@ -54,7 +54,7 @@
                        class="form-control"
                        value="<?= isset($modelo) ? $modelo->getValor() : ''; ?>">
             </div>
-            <div class="col col-md-auto">
+            <div class="col col-md-3 me-5">
                 <label for="cod-barras">Cod.Barras</label>
                 <input type="number"
                        id="cod-barras"
@@ -62,7 +62,7 @@
                        class="form-control"
                        value="<?= isset($modelo) ? $modelo->getCodBarras() : ''; ?>">
             </div>
-            <div class="col col-md-auto">
+            <div class="col col-md-3 mx-5">
                 <label for="data-entrada">Entrada</label>
                 <div>
                 <input type="date"
@@ -76,7 +76,7 @@
                 </div>
             </div>
             <?php if(isset($modelo) && $modelo->getDataSaida() != null) : ?>
-            <div class="col col-md-auto">
+            <div class="col col-md-3 mx-5">
                 <label for="data-entrada">Saída</label>
                 <div>
                     <input type="datetime-local"
