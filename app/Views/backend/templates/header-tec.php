0114b381e312a3a994ccd9e2822169ae00f62705
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-05-16 19:14:26 -0300

    Tamanho do campo de busca ajustado

--- app/Views/backend/templates/header-tec.php
+++ app/Views/backend/templates/header-tec.php
@@ -166,46 +166,39 @@
 
             <!-- Topbar -->
             <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
                 <!-- Sidebar Toggle (Topbar) -->
                 <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                     <i class="fa fa-bars"></i>
                 </button>
 
                 <!-- Topbar Navbar -->
-                <ul class="navbar-nav ml-auto">
-
-                    <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 text-gray-600"><?= $title ?></span>
-                        </a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <span class="mx-4 text-gray-600"><?= $title ?></span>
                     </li>
+                </ul>
 
-                    <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                    <li class="nav-item dropdown no-arrow">
+                <ul class="navbar-nav ml-auto">
+                    <li  class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
                             <i class="fas fa-search fa-fw"></i>
                         </a>
                         <!-- Dropdown - Messages -->
-                        <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                        <div style="width: 15rem" class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
                             <form class="form-inline w-100 navbar-search">
                                 <div class="input-group">
-                                    <input type="text" class="form-control bg-light border-0 small" placeholder="Procurando por..." aria-label="Search" aria-describedby="basic-addon2">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary" type="button">
+                                    <div class="input-group-prepend">
+                                        <button class="btn btn-primary btn-sm" type="button">
                                             <i class="fas fa-search fa-sm"></i>
                                         </button>
                                     </div>
+                                    <input type="text" class="form-control form-control-sm bg-light border-0 small" placeholder="Procurando por..." aria-label="Search" aria-describedby="basic-addon2">
                                 </div>
                             </form>
                         </div>
                     </li>
 
-
-                    <div class="topbar-divider d-none d-sm-block"></div>
-
-                    <!-- Nav Item - User Information -->
-                    <li class="nav-item dropdown no-arrow">
+                    <li class="nav-item dropdown no-arrow ml-auto">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= session()->get('usuario')['usuario'] ?></span>
                             <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60" alt="">
@@ -235,9 +228,8 @@
                             </a>
                         </div>
                     </li>
-
                 </ul>
-
             </nav>
+
             <!-- End of Topbar -->
             <div class="container-fluid row">
