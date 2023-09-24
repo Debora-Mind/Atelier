3de87a90623ee21f388733e11de205781ea3f135
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-09-23 22:37:27 -0300

    Alterando acesso axios

--- frontend/src/services/http.ts
+++ frontend/src/services/http.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'https://atelier-pearl.vercel.app/',
     headers: {
         'Content-type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
-        'Access-Control-Allow-Origin': 'http://localhost:8081'
+        'Access-Control-Allow-Origin': 'https://atelier-pearl.vercel.app/'
     },
     withCredentials: true
 
