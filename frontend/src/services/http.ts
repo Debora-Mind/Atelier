450801752d9c438002a4349282e784b70e76181a
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-09-24 22:09:36 -0300

    Ajuste axios

--- frontend/src/services/http.ts
+++ frontend/src/services/http.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://atelier-pearl.vercel.app/',
+    baseURL: 'https://atelier-8ws4hjml4-debora-mind.vercel.app',
     headers: {
         'Content-type': 'application/json',
         'X-Requested-With': 'XMLHttpRequest',
-        'Access-Control-Allow-Origin': ['http://localhost:8081', 'https://atelier-pearl.vercel.app/']
+        'Access-Control-Allow-Origin': ['http://localhost:8081', 'https://atelier-pearl.vercel.app/', 'https://atelier-8ws4hjml4-debora-mind.vercel.app']
     },
     withCredentials: true
 
