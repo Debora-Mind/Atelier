0235076dd01c95414af035d3cb81c7596ff557eb
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-09-29 21:07:18 -0300

    Ajustes vercel

--- installation.sh
+++ installation.sh
@@ -5,11 +5,17 @@ cd frontend
 echo "Executando: npm install"
 npm install
 
+echo "Executando: vue-cli-service build"
+vue-cli-service build
+
 # Volte uma pasta para acessar a pasta raiz
 cd ..
 cd backend
 
 echo "Executando: composer update"
 composer update
+
+echo "Executando: php spark serve"
+php spark serve
 
 echo "Script de implantação concluído."
