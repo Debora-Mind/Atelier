d3bcad896a4b8c013f97defc68baea0f2a7ed686
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-10-02 19:45:25 -0300

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-  outputDir: 'docs', // Isso define a pasta de saída como "docs"
-  // ...
+  publicPath: '../docs',
+  outputDir: '../docs',
 };
