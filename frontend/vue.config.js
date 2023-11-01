cdd5a90a8fca2958315ea6c7905bb90298103494
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-11-01 09:04:48 -0300

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
-      swSrc: 'src/registerServiceWorker.ts',
+      swSrc: '/src/registerServiceWorker.ts',
       // ...other Workbox options...
     }
   }
