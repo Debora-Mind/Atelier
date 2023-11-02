52d8968c2006965d8669d3d9e28a0bdf202396fb
Débora Almeida <debora.almeida.de.mello@gmail.com>
2023-11-02 17:18:37 -0300

    Adicionada verificação de servidor no login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -55,6 +55,7 @@ export default {
           })
           .catch(e => {
             console.error('Erro ao enviar dados:', e);
+            this.erroValidacao['usuario'] = 'Falha na comunicação com o servidor.'
           });
       if (sessionStorage.getItem('logado') === 'true') {
         window.location.reload();
